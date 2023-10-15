cd43b4ef892571e6e05fb61f0079d9a978c98ef6
aionclassic <146429880+aionclassic@users.noreply.github.com>
2023-10-15 20:41:16 +0300

    2 update

--- main.py
+++ main.py
@@ -4,27 +4,39 @@ import os
 
 name_column = 'number'  # название столбца для пересечения
 
-# создать интерфейс выбора папки в которой находятся файлы
-# file_list = glob.glob('data_dir/*xlsx')
+#создает список файлов находящихся в папке переменная data_directory
 
-# сделать чтобы каждый фрейм создавался на находящийся в папке(file_list) xlsx фаил
-df1 = pd.read_excel('test_exel_1.xlsx')  # файл по 500000 рандомных 10 цыфр
-df2 = pd.read_excel('test_exel_2.xlsx')  # файл по 500000 рандомных 10 цыфр
-df3 = pd.read_excel('test_exel_3.xlsx')  # файл по 500000 рандомных 10 цыфр
-df4 = pd.read_excel('test_exel_4.xlsx')  # файл по 500000 рандомных 10 цыфр
 
-# оставляем только уникальные элементы
-mylist1 = set(df1[name_number].tolist())
-mylist2 = set(df2[name_number].tolist())
-mylist3 = set(df3[name_number].tolist())
-mylist4 = set(df4[name_number].tolist())
+#создаем переменные df_1...N с путями к N файлам из папки
+list_file = []
+data_directory = '111'
+for root, dirs, files in os.walk(data_directory):
+    for filename in files:
+        list_file.append(os.path.join(root, filename))
+
+#создаем преременную dfN с 1ё колонкой , создаем из этой переменно переменую с уникальными занчениями вколонке
+ii=0
+iii=[]
+for i in list_file:
+    ii += 1
+    globals()[f'df{ii}'] = pd.read_excel(i)
+    globals()[f'set_data_df{ii}'] = set(eval(f'df{ii}[name_column]'))
+    iii.append(f'set_data_df{ii}')
 
 start_time = time.time()
 
-# написать функцию которая пресекает фреймы в зависимости от их количества
-end_result = mylist1 & mylist2 & mylist3 & mylist4
+# функцию которая пресекает фреймы в зависимости от их количества
+end_result =[]
+for xxx in iii:
+    end_result.extend(xxx+' &')
 
-print(end_result)
+#удаляем последний элемент & и собираем в строку
+end_result.pop(-1)
+end_result_data=eval("".join(end_result))
+
+
+print(end_result_data)
+
 
 end_time = str(time.time() - start_time)[:-15] + ' сек'
 
