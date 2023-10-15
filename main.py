96973f6624406a0e436af713d580d63d934c6b63
aionclassic <146429880+aionclassic@users.noreply.github.com>
2023-10-15 23:07:28 +0300

    2 update

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import time
 import os
-
+from tkinter import filedialog
+from tkinter import *
+import xlsxwriter
 name_column = 'number'  # название столбца для пересечения
 
 #создает список файлов находящихся в папке переменная data_directory
 
+root = Tk()
+root.withdraw()
+data_directory = filedialog.askdirectory()
+
 
 #создаем переменные df_1...N с путями к N файлам из папки
 list_file = []
-data_directory = '111'
+#data_directory = '111'
 for root, dirs, files in os.walk(data_directory):
     for filename in files:
         list_file.append(os.path.join(root, filename))
@@ -37,7 +43,18 @@ end_result_data=eval("".join(end_result))
 
 print(end_result_data)
 
+#запись результата в фаил
+df55 = pd.DataFrame(end_result_data)
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter("pandas_simple.xlsx", engine="xlsxwriter")
+# Convert the dataframe to an XlsxWriter Excel object.
+df55.to_excel(writer, index= False, header= False)
+# Close the Pandas Excel writer and output the Excel file.
+writer.close()
+
+
 
 end_time = str(time.time() - start_time)[:-15] + ' сек'
 
 print(end_time)
+
